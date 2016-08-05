a34f21f82c6152c20d046989a32520276837ddb0
Maxim Grishkov <grishkov.m@gmail.com>
2016-08-06 00:41:40 +0300

    Форма 1. Start page, подготовил шаблон

--- main/app/modules/core/coreModule.js
+++ main/app/modules/core/coreModule.js
@@ -1,11 +1,18 @@
+(function() {
 angular.module('costAnswer.core', ['costAnswer.core.controllers']);
 angular.module('costAnswer.core')
     .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
         $stateProvider
-            .state('description', {
+            .state('descriptionCore', {
                 url: '/description/:costingMethod',
-                controller: 'descriptionController',
+                controller: 'descriptionCoreController',
                 templateUrl: 'app/modules/core/views/description.html'
+            })
+            .state('startCore', {
+                url: '/start',
+                controller: 'startCoreController',
+                templateUrl: 'app/modules/core/views/start.html'
             });
         $urlRouterProvider.otherwise('/description/standard-costing');
     }]);
+}());
