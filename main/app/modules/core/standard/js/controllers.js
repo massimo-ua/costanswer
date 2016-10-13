c8d0569f8ee1d7b250172a32f4f3c2db227b1799
Maxim Grishkov <grishkov.m@gmail.com>
2016-10-14 01:37:45 +0300

    Добавил кнопку для создания новых продуктов (без подключения к БД)

--- main/app/modules/core/standard/js/controllers.js
+++ main/app/modules/core/standard/js/controllers.js
@@ -2,11 +2,39 @@
     angular.module('costAnswer.core.standard.controllers',[]);
     angular.module('costAnswer.core.standard.controllers')
     .controller('standardController', ['$scope', 'ngDialog', function($scope, ngDialog){
-        $scope.addNewProduct = function() {
-            ngDialog.open({
-                template: '<p>Standard Costing Home</p>',
-                plain: true
-            });
+        function init() {
+            $scope.nameProperty = "name";
+            $scope.itemsList = [];
+            $scope.form = {};
+            $scope.controls = {
+                buttonText: "Add",
+                confirmButtonText: "Save",
+                addButtonActive: false
+            };
+        }
+        function resetForm() {
+            $scope.form = {};
+            $scope.controls.addButtonActive = false;
+        }
+        init();
+        $scope.addProduct = function() {
+            ngDialog.openConfirm({
+                        template: 'app/modules/core/standard/views/dialogs/add-product.html',
+                        className: 'ngdialog-theme-plain',
+                        closeByDocument: false,
+                        closeByEscape: false,
+                        showClose: true,
+                        scope: $scope
+            })
+            .then(
+                function(value) {
+                    console.log(value);
+                    $scope.itemsList.push(value);
+                    resetForm();
+                },
+                function(value){
+                    console.log(value);
+                });
         }
     }]);
     angular.module('costAnswer.core.standard.controllers')
