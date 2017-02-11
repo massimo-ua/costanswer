06a948542e335c8d0c6c72a0ae2d0b82d435a328
Maxim Grishkov <grishkov.m@gmail.com>
2017-02-11 15:41:33 +0200

    сохранение параметров в moh.settings

--- main/app/modules/core/moh/js/controllers/settings.controller.js
+++ main/app/modules/core/moh/js/controllers/settings.controller.js
@@ -3,55 +3,53 @@
     //controller functions definitions
     function MohSettingsController($scope, $log, $localStorage, MOH_ALLOCATION_BASE, MOH_CALCULATION_BASE, DataModel) {
         var vm = this;
-        vm.saveCalculationBase = function() {
-            if($localStorage.moh && $localStorage.uuid && vm.mohSettings.calculation_base_id != undefined) {
-                var moh = {};
+        vm.saveSettings =  function() {
+            if(!$localStorage.moh) {
+                vm.buttonText = "Saving...";
+                if(!$localStorage.uuid || vm.mohSettings.allocation_base_id == undefined || vm.mohSettings.calculation_base_id == undefined) return;
+                var moh = new DataModel.Moh();
                 moh.project_uuid = $localStorage.uuid;
                 moh.calculation_base_id = vm.mohSettings.calculation_base_id;
-                $log.debug(moh);
-                DataModel.Moh.updateWithUuid(moh)
-                    .$promise
-                        .then(function(){
-                            $scope.$emit('MOH_CALCULATION_CHANGE', vm.mohSettings.calculation_base_id);
+                moh.allocation_base_id = vm.mohSettings.allocation_base_id;
+                moh.por_rate = vm.mohSettings.por_rate / 100;
+                moh.$saveWithUuid(function(response){
+                    $localStorage.moh = response.id;
+                    vm.buttonText = "Update";
+                    $scope.$emit('MOH_CALCULATION_CHANGE', response.calculation_base_id);
+                },
+                function(err){
+                    vm.buttonText = "Save";
+                    $log.debug(err);
+                });
+            } else {
+                if(!$localStorage.uuid || vm.mohSettings.allocation_base_id == undefined) return;
+                    vm.buttonText = "Updating...";
+                    var moh = {};
+                    moh.project_uuid = $localStorage.uuid;
+                    moh.allocation_base_id = vm.mohSettings.allocation_base_id;
+                    moh.calculation_base_id = vm.mohSettings.calculation_base_id;
+                    moh.por_rate = vm.mohSettings.por_rate / 100;
+                    DataModel.Moh.updateWithUuid(moh).$promise
+                        .then(function(response){
+                            $scope.$emit('MOH_CALCULATION_CHANGE', response.calculation_base_id);
                         })
                         .catch(function(err){
                             $log.debug(err);
+                        })
+                        .finally(function(){
+                            vm.buttonText = "Update";
                         });
-                        }
-                        return;
                 }
-                vm.saveAllocationBase = function() {
-                    if(!$localStorage.moh) {
-                        if(!$localStorage.uuid || vm.mohSettings.allocation_base_id == undefined || vm.mohSettings.calculation_base_id == undefined) return;
-                        var moh = new DataModel.Moh();
-                        moh.project_uuid = $localStorage.uuid;
-                        moh.calculation_base_id = vm.mohSettings.calculation_base_id;
-                        moh.allocation_base_id = vm.mohSettings.allocation_base_id;
-                        moh.$saveWithUuid(function(response){
-                            $localStorage.moh = response.id;
-                        },
-                        function(err){
-                            $log.debug(err);
-                        });
-                    } else {
-                        if(!$localStorage.uuid || vm.mohSettings.allocation_base_id == undefined) return;
-                        var moh = {};
-                        moh.project_uuid = $localStorage.uuid;
-                        moh.allocation_base_id = vm.mohSettings.allocation_base_id;
-                        DataModel.Moh.updateWithUuid(moh).$promise
-                            .catch(function(err){
-                                $log.debug(err);
-                            });
-                    }
-
-                }
+            }
                 function init() {
                     vm.mohSettings = {};
                     vm.uuid = $localStorage.uuid;
+                    vm.buttonText = $localStorage.moh ? "Update" : "Save";
                     if(vm.uuid != undefined) {
                         DataModel.Moh.getWithUuid({ uuid: vm.uuid }, function(response){
                             vm.mohSettings.calculation_base_id = parseInt(response.calculation_base_id);
                             vm.mohSettings.allocation_base_id = parseInt(response.allocation_base_id);
+                            vm.mohSettings.por_rate = response.por_rate * 100;
                             $scope.$emit('MOH_CALCULATION_CHANGE', vm.mohSettings.calculation_base_id);
                         });
                     }
