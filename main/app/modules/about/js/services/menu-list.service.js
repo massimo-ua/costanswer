ac93245d8558ba2e136aa9bdb7c99b597e011a58
Maxim Grishkov <grishkov.m@gmail.com>
2017-05-09 22:45:01 +0300

    Новая задача: изменил порядок элементов меню в секции about

--- main/app/modules/about/js/services/menu-list.service.js
+++ main/app/modules/about/js/services/menu-list.service.js
@@ -6,23 +6,23 @@
         return {
         list: function() {
                 return [
-                    {"id": 1, "name": "Checkups", "sref": "about.checkups", "iconClass": "ion-settings", "disabled": false},
-                    {"id": 2, "name": "Cost analysis", "sref": "about.cost-analysis", "iconClass": "ion-ios-home", "disabled": false},
-                    {"id": 3, "name": "Costing methods", "sref": "about.costing-methods", "iconClass": "ion-social-buffer", "disabled": false},
-                    {"id": 4, "name": "Direct vs Indirect", "sref": "about.direct-vs-indirect", "iconClass": "ion-connection-bars", "disabled": false},
-                    {"id": 5, "name": "FMOH", "sref": "about.fmoh", "iconClass": "ion-ios-alarm-outline", "disabled": false},
-                    {"id": 6, "name": "Forecast vs actual", "sref": "about.forecast-vs-actual", "iconClass": "ion-paintbucket", "disabled": false},
-                    {"id": 7, "name": "Inventory accounting", "sref": "about.inventory-accounting", "iconClass": "ion-ios-people", "disabled": false},
-                    {"id": 8, "name": "Job order costing", "sref": "about.job-order-costing", "iconClass": "ion-ios-pie", "disabled": false},
-                    {"id": 9, "name": "Per batch approach", "sref": "about.per-batch-approach", "iconClass": "ion-ios-cog", "disabled": false},
-                    {"id": 10, "name": "Process costing", "sref": "about.process-costing", "iconClass": "ion-ios-alarm", "disabled": false},
-                    {"id": 11, "name": "Product vs service", "sref": "about.product-vs-service", "iconClass": "ion-pricetags", "disabled": false},
-                    {"id": 12, "name": "Right method", "sref": "about.right-method", "iconClass": "ion-clipboard", "disabled": false},
-                    {"id": 13, "name": "Standard costing", "sref": "about.standard-costing", "iconClass": "ion-clipboard", "disabled": false},
-                    {"id": 14, "name": "Theory in brief", "sref": "about.theory-in-brief", "iconClass": "ion-clipboard", "disabled": false},
-                    {"id": 15, "name": "Variable vs Fixed", "sref": "about.variable-vs-fixed", "iconClass": "ion-clipboard", "disabled": false},
-                    {"id": 16, "name": "VMOH", "sref": "about.vmoh", "iconClass": "ion-clipboard", "disabled": false},
-                    {"id": 17, "name": "What is costanswer", "sref": "about.what-is-costanswer", "iconClass": "ion-clipboard", "disabled": false}
+                    {"id": 1, "name": "What is COSTANSWER", "sref": "about.what-is-costanswer", "iconClass": "ion-settings", "disabled": false},
+                    {"id": 2, "name": "Theory in brief", "sref": "about.theory-in-brief", "iconClass": "ion-ios-home", "disabled": false},
+                    {"id": 3, "name": "Direct vs indirect", "sref": "about.direct-vs-indirect", "iconClass": "ion-social-buffer", "disabled": false},
+                    {"id": 4, "name": "Variable vs fixed", "sref": "about.variable-vs-fixed", "iconClass": "ion-connection-bars", "disabled": false},
+                    {"id": 5, "name": "Product cost vs service cost", "sref": "about.product-cost-vs-service-cost", "iconClass": "ion-ios-alarm-outline", "disabled": false},
+                    {"id": 6, "name": "Inventory accounting", "sref": "about.inventory-accounting", "iconClass": "ion-paintbucket", "disabled": false},
+                    {"id": 7, "name": "Costing methods", "sref": "about.costing-methods", "iconClass": "ion-ios-people", "disabled": false},
+                    {"id": 8, "name": "Standard costing", "sref": "about.standard-costing", "iconClass": "ion-ios-pie", "disabled": false},
+                    {"id": 9, "name": "Process costing", "sref": "about.process-costing", "iconClass": "ion-ios-cog", "disabled": false},
+                    {"id": 10, "name": "Job order costing", "sref": "about.job-order-costing", "iconClass": "ion-ios-alarm", "disabled": false},
+                    {"id": 11, "name": "Your costing method", "sref": "about.your-costing-method", "iconClass": "ion-pricetags", "disabled": false},
+                    {"id": 12, "name": "Forecast vs actual", "sref": "about.forecast-vs-actual", "iconClass": "ion-clipboard", "disabled": false},
+                    {"id": 13, "name": "\"Per batch\" approach", "sref": "about.per-batch-approach", "iconClass": "ion-clipboard", "disabled": false},
+                    {"id": 14, "name": "Variable overhead", "sref": "about.variable-overhead", "iconClass": "ion-clipboard", "disabled": false},
+                    {"id": 15, "name": "Fixed overhead", "sref": "about.fixed-overhead", "iconClass": "ion-clipboard", "disabled": false},
+                    {"id": 16, "name": "Cost analysis", "sref": "about.cost-analysis", "iconClass": "ion-clipboard", "disabled": false},
+                    {"id": 17, "name": "Check ups", "sref": "about.check-ups", "iconClass": "ion-clipboard", "disabled": false}
                 ];
             }
         };
