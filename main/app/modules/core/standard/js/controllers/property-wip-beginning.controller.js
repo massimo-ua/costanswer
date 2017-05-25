6b31abd9c3736cb92a5ac91f8f4f2412aeb4a687
Maxim Grishkov <grishkov.m@gmail.com>
2017-05-26 00:48:36 +0300

    внес изменения wip_beginning

---
+++ main/app/modules/core/standard/js/controllers/property-wip-beginning.controller.js
@@ -0,0 +1,95 @@
+(function(){
+    'use strict';
+    angular.module('costAnswer.core.standard.controllers')
+        .component('propertyWipBeginning', {
+            restrict: 'E',
+            templateUrl: 'app/modules/core/standard/views/property/wip-beginning.html',
+            controller: propertyWipBeginningController
+        });
+    function propertyWipBeginningController($log, $localStorage, $stateParams, DataModel, monthService, standardService) {
+        var vm = this;
+        //$log.error('singleProductController');
+        function init() {
+            vm.product_id = $stateParams.id;
+            vm.form = {};
+            vm.month_number = 1;
+            vm.year_number = 1;
+            vm.instantReport = [];
+            vm.reportId = 'wip';
+            vm.controls = {
+                buttonText: "Save",
+                //nameMain: "Work in process (WIP) beginning",
+                namePlaceholder: "$",
+                nameErrorText: "Please, fill in amount of beginning WIP costs (0 allowed)",
+                Name: "WIP beginning $",
+                Qty: "WIP beginning Units",
+                qtyPlaceholder: "Units",
+                qtyErrorText: "Please, fill in WIP beginning Units (0 allowed)",
+            };
+            DataModel.Product.getWip({ id: vm.product_id })
+                .$promise
+                    .then(function(response){
+                        vm.id = response[0].id;
+                        vm.controls.buttonText = "Update";
+                        vm.form.beginning_costs = parseInt(response[0].beginning_costs) / 100;
+                        vm.form.beginning_quantity = parseInt(response[0].beginning_quantity) / 100;
+                    })
+                    .catch(function(){
+                        vm.id = undefined;            
+                    });
+            if($localStorage.uuid !== undefined) {
+                DataModel.Project.uuid({ uuid: $localStorage.uuid })
+                    .$promise
+                        .then(function(response){
+                            vm.project = response;
+                            vm.controls.namePlaceholder = monthService.Month(response.begin_month).full + ',' + vm.controls.namePlaceholder;
+                        });
+            }
+            refreshReport();
+        }
+        function refreshReport() {
+            if(vm.product_id) {
+                standardService.getInstantReport(vm.product_id, vm.reportId, function(response){
+                    vm.instantReport = response;
+                });
+            }
+            return;
+        }
+        init();
+        vm.onSave = function(form) {
+            var wip = new DataModel.Product();
+            if(vm.id === undefined) {
+                vm.controls.buttonText = "Saving...";
+                wip.beginning_costs = Math.round(vm.form.beginning_costs * 100);
+                wip.beginning_quantity = Math.round(vm.form.beginning_quantity * 100);
+                wip.month_number = vm.month_number;
+                wip.year_number = vm.year_number;
+                wip.$saveWip({ id: vm.product_id })
+                    .then(function(response){
+                        vm.controls.buttonText = "Update";
+                        vm.id = response.id;
+                        refreshReport();
+                    })
+                    .catch(function(error){
+                        vm.controls.buttonText = "Save";
+                    });
+            }
+            else {
+                vm.controls.buttonText = "Updating...";
+                wip.beginning_costs = Math.round(vm.form.beginning_costs * 100);
+                wip.beginning_quantity = Math.round(vm.form.beginning_quantity * 100);
+                wip.$updateWip({ id: vm.id })
+                    .then(function(response){
+                        refreshReport();
+                    })
+                    .catch(function(error){
+                        $log.error(error);
+                    })
+                    .finally(function(){
+                        vm.controls.buttonText = "Update";
+                    });
+            }
+        };
+    }
+    propertyWipBeginningController.$inject = ['$log', '$localStorage', '$stateParams', 'DataModel', 'monthService', 'standardService'];
+}());
