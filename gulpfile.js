8b746583801b2d157ef5278a3083954f51e401df
Maxim Grishkov <grishkov.m@gmail.com>
2017-07-30 00:27:54 +0300

    minification test

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,28 @@
 var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify'),
     browserSync = require('browser-sync').create();
 
-gulp.task('run-dev', function(){
+    //script paths
+    var jsFiles = 'main/app/js/services/currency.service.js',
+        jsDest = 'main/app/dist/js';
+
+    gulp.task('scripts', function() {
+        return gulp.src(jsFiles)
+            .pipe(concat('bundle.js'))
+            .pipe(rename('bundle.min.js'))
+            .pipe(uglify())
+            .pipe(gulp.dest(jsDest));
+    });
+
+/*gulp.task('run-dev', function(){
     browserSync.init({
         server: {
             baseDir: '.'
         },
         port: 7777
     });
-});
+});*/
 
-gulp.task('default', ['run-dev']);
+gulp.task('default', ['scripts']);
