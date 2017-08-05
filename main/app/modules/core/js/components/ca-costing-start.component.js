5944ffc644793d6be1acadfe32584e184c3ada24
Maxim Grishkov <grishkov.m@gmail.com>
2017-08-05 13:18:18 +0300

    домашняя страница для процесса в продукте

--- main/app/modules/core/js/components/ca-costing-start.component.js
+++ main/app/modules/core/js/components/ca-costing-start.component.js
@@ -3,17 +3,17 @@
     angular.module('costAnswer.core.components')
         .component('caCostingStart', {
             bindings: {
-                nextSref: '@',
-                buttonText: '@?'
+                nextSref: '<',
+                buttonText: '<?'
             },
             templateUrl: 'app/modules/core/views/ca-costing-start.html',
             controller: caCostingStartController
         });
     function caCostingStartController($log) {
         var vm = this;
-        vm.$onInit(function(){
+        vm.$onInit = function(){
             vm.buttonValue = vm.buttonText || 'Create product or service';
-        });
+        };
     }
     caCostingStartController.$inject = ['$log'];
 })();
