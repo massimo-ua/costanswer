72e0ad5fe99f3852abaf3ff1e5d89814c6cb47ce
Maxim Grishkov <grishkov.m@gmail.com>
2017-08-11 23:38:01 +0300

    angular formly configuration

--- main/app/js/app.js
+++ main/app/js/app.js
@@ -71,7 +71,7 @@
                 template:
                     [
                         '<div class="col-xs-12 col-md-4 col-lg-4 col-md-offset-4 col-lg-offset-4 form-group text-center" ng-class="{\'has-error\': options.formControl.$dirty && options.formControl.$invalid}">',
-                        '<label class="control-label" for="{{::id}}">{{to.required ? \'*\' : \'\'}}{{to.label}}:</label>',
+                        '<label class="control-label" for="{{::id}}">{{to.label}}:</label>',
                         '<formly-transclude></formly-transclude>',
                         '<div class="help-block error" ng-show="options.validation.errorExistsAndShouldBeVisible">{{ to.errorText }}</div>',
                         '</div>'
