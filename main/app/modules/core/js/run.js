31384845e391180cb2734aa955128b6cc0290a33
Maxim Grishkov <grishkov.m@gmail.com>
2017-08-21 23:52:30 +0300

    process production plan

--- main/app/modules/core/js/run.js
+++ main/app/modules/core/js/run.js
@@ -7,23 +7,12 @@
             name: 'input',
             template: '<input class="form-control costanswer" ng-model="model[options.key]" />'
         });
+        formlyConfig.setType({
+            name: 'annualMonthly',
+            templateUrl: 'app/modules/core/views/annual-monthly.html'
+        });
         formlyConfig.setWrapper([
             {
-                /*template: [
-                    '<div class="form-group"',
-                    'ng-class="{\'has-error\': options.formControl.$invalid}">',
-                    '<label for="{{::id}}">{{options.templateOptions.required ? \'*\' : \'\'}}{{options.templateOptions.label}}</label>',
-                    '<formly-transclude></formly-transclude>',
-                    '<div class="validation"',
-                    //'ng-if="options.validation.errorExistsAndShouldBeVisible"',
-                    'ng-messages="options.formControl.$error">',
-                    '<div ng-messages-include="validation.html"></div>',
-                    '<div ng-message="{{::name}}" ng-repeat="(name, message) in ::options.validation.messages">',
-                    '{{message(options.formControl.$viewValue, options.formControl.$modelValue, this)}}',
-                    '</div>',
-                    '</div>',
-                    '</div>'
-                ].join(' '),*/
                 template:
                     [
                         '<div class="col-xs-12 col-md-4 col-lg-4 col-md-offset-4 col-lg-offset-4 form-group text-center m-t-10px" ng-class="{\'has-error\': options.formControl.$dirty && options.formControl.$invalid}">',
@@ -33,6 +22,12 @@
                         '</div>'
                     ].join(' '),
                 types: 'input'
+            },
+            {
+                template: [
+
+                ].join(' '),
+                types: 'annualMonthly'
             }
         ]);
     }
