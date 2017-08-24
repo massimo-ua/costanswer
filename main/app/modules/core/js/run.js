3db17794f78766e85b2a50629f38542d64a61f0c
Maxim Grishkov <grishkov.m@gmail.com>
2017-08-24 19:49:23 +0300

    process production plan

--- main/app/modules/core/js/run.js
+++ main/app/modules/core/js/run.js
@@ -39,7 +39,7 @@
                 '<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 text-center">',
                 '<fieldset ng-disabled="controls.formDisabled">',
                 '<div ng-show="to.label" class="col-xs-12 col-md-12 col-lg-12 text-center">',
-                '<label class="control-label m-b-20px" for="{{::id}}">{{to.label}}:</label>',
+                '<label class="control-label m-t-10px" for="{{::id}}">{{to.label}}:</label>',
                 '</div>',
                 '<formly-transclude></formly-transclude>',
                 '</fieldset>',
