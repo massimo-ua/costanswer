b693027649bff4fe73107c9e6adce61dc5aae017
Maxim Grishkov <grishkov.m@gmail.com>
2017-08-29 00:00:53 +0300

    fix for dm layout

--- main/app/modules/core/js/run.js
+++ main/app/modules/core/js/run.js
@@ -32,6 +32,17 @@
             {
                 template:
                     [
+                        '<div class="form-group text-center m-t-10px" ng-class="{\'has-error\': options.formControl.$dirty && options.formControl.$invalid}">',
+                        '<label class="control-label m-t-10px" for="{{::id}}">{{to.label}}:</label>',
+                        '<formly-transclude></formly-transclude>',
+                        '<div class="help-block error" ng-show="options.validation.errorExistsAndShouldBeVisible">{{ to.errorText }}</div>',
+                        '</div>'
+                    ].join(' '),
+                types: 'ca-input-ww'
+            },
+            {
+                template:
+                    [
                         '<div class="col-xs-12 col-md-4 col-lg-4 col-md-offset-4 col-lg-offset-4 form-group text-center m-t-10px" ng-class="{\'has-error\': options.formControl.$dirty && options.formControl.$invalid}">',
                         '<label class="control-label m-t-10px" for="{{::id}}">{{to.label}}:</label>',
                         '<formly-transclude></formly-transclude>',
