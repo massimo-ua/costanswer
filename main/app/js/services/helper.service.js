1aa7a66f8a93da0f58784b47db4eb55a82dd035d
Maxim Grishkov <grishkov.m@gmail.com>
2017-11-14 00:58:40 +0200

    Обмен сообщениями через HelperService

--- main/app/js/services/helper.service.js
+++ main/app/js/services/helper.service.js
@@ -44,7 +44,6 @@
         };
         factory.getValue = function(key) {
             if(storage[key]) return storage[key];
-            else return undefined;
         };
         return factory;
     }
