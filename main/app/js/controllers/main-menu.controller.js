7283873c8d3f46dc6e90bd42f7b5f448333c3d16
Maxim Grishkov <grishkov.m@gmail.com>
2017-12-11 22:41:44 +0200

    Добавил ng-bootstrap-submenu, написал контроллер

--- main/app/js/controllers/main-menu.controller.js
+++ main/app/js/controllers/main-menu.controller.js
@@ -8,10 +8,8 @@
                 { display: 'Child 1', href: '#', children: [
                 { display: 'Sub 1', href: '#sub1', children: []},
                 { display: 'Sub 2', href: '#sub2', children: []}]},
-                { display: 'Child 2', href: '#child2', children: []}]},
-            { display: 'Dropdown Item 2', href: '#dropdown2', children: []},
-            { display: 'Dropdown Item 3', href: '#', children: [
-                { display: 'Child 3', href: '#child3', children: []}]}
+                { display: 'Child 2', href: '#child2', children: []}]
+            }
             ];
     }
     //controller function linking
