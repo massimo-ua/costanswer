22b0609bdd31551f20795bdb97c87f7364aeab2b
Maxim Grishkov <grishkov.m@gmail.com>
2017-12-11 23:15:14 +0200

    Добавил ng-bootstrap-submenu, написал контроллер

--- main/app/js/controllers/main-menu.controller.js
+++ main/app/js/controllers/main-menu.controller.js
@@ -1,19 +1,30 @@
 (function(){
-    'use strict'
-    function mainMenuController() {
-        var vm = this;
-        vm.$onInit = function() {
-            vm.menuItems = [
-                { display: 'Dropdown Item 1', href: '#', children: [
-                    { display: 'Child 1', href: '#', children: [
-                    { display: 'Sub 1', href: '#sub1', children: []},
-                    { display: 'Sub 2', href: '#sub2', children: []}]},
-                    { display: 'Child 2', href: '#child2', children: []}]
-                }
-            ];
-        };
-    }
-    //controller function linking
+    'use strict';
     angular.module('costAnswer.controllers')
-        .controller('mainMenuController', mainMenuController)
+        .component('caMainMenu', {
+            restrict: 'E',
+            templateUrl: 'app/views/main-menu.html',
+            controller: caMainMenuController
+        });
+        function caMainMenuController() {
+            var vm = this;
+            vm.$onInit = function() {
+                vm.menuItems = [
+                    { display: 'Dropdown Item 1', href: '#', children: [
+                      { display: 'Child 1', href: '#', children: [
+                        { display: 'Sub 1', href: 'http://www.google.com', children: []},
+                        { display: 'Sub 2', href: '#', children: [
+                          { display: 'Grand Child 1', href: 'http://www.google.com', children: []},
+                          { display: 'Grand Child 2', href: 'http://www.google.com', children: []}
+                          ]}
+                        ]},
+                      { display: 'Child 2', href: 'http://www.google.com', children: []}
+                      ]},
+                    { display: 'Dropdown Item 2', href: 'http://www.google.com', children: []},
+                    { display: 'Dropdown Item 3', href: '#', children: [
+                      { display: 'Child 3', href: 'http://www.google.com', children: []}
+                      ]}
+                    ];
+            };
+        }
 }());
