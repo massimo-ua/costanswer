7fdb37013a88f71f589498e7f32d7fc1a1fa8fbc
Maxim Grishkov <grishkov.m@gmail.com>
2018-03-05 23:39:20 +0200

    upgrade from bower to yarn

--- main/app/modules/manual/manualModule.js
+++ main/app/modules/manual/manualModule.js
@@ -41,64 +41,64 @@
             })
            .state('manual-standard-costing', {
                 url: '/manual-standard-costing',
-                redirectTo: 'manual-standard-costing.inventory',
+                redirectTo: 'manual-standard-costing/inventory',
                 views: {
                     "main": {
                         template: '<manual-standard-costing></manual-standard-costing>'
                     }
                 }
             })
-            .state('manual-standard-costing.inventory', {
+            .state('manual-standard-costing/inventory', {
                 url: '/inventory',
                 parent: 'manual-standard-costing',
                 templateUrl: 'app/modules/manual/views/standard-costing-inventory.html',
             })
-            .state('manual-standard-costing.machine-hours', {
+            .state('manual-standard-costing/machine-hours', {
                 url: '/machine-hours',
                 parent: 'manual-standard-costing',
                 templateUrl: 'app/modules/manual/views/standard-costing-machine-hours.html',
             })
-            .state('manual-standard-costing.markup', {
+            .state('manual-standard-costing/markup', {
                 url: '/markup',
                 parent: 'manual-standard-costing',
                 templateUrl: 'app/modules/manual/views/standard-costing-markup.html',
             })
-            .state('manual-standard-costing.vmoh', {
+            .state('manual-standard-costing/vmoh', {
                 url: '/vmoh',
                 parent: 'manual-standard-costing',
                 templateUrl: 'app/modules/manual/views/standard-costing-vmoh.html',
             })
-            .state('manual-standard-costing.wip-ending', {
+            .state('manual-standard-costing/wip-ending', {
                 url: '/wip-ending',
                 parent: 'manual-standard-costing',
                 templateUrl: 'app/modules/manual/views/standard-costing-wip-ending.html',
             })
-            .state('manual-standard-costing.direct-labor', {
+            .state('manual-standard-costing/direct-labor', {
                 url: '/direct-labor',
                 parent: 'manual-standard-costing',
                 templateUrl: 'app/modules/manual/views/standard-costing-direct-labor.html',
             })
-            .state('manual-standard-costing.direct-materials', {
+            .state('manual-standard-costing/direct-materials', {
                 url: '/direct-materials',
                 parent: 'manual-standard-costing',
                 templateUrl: 'app/modules/manual/views/standard-costing-direct-materials.html',
             })
-            .state('manual-standard-costing.product-creation', {
+            .state('manual-standard-costing/product-creation', {
                 url: '/product-creation',
                 parent: 'manual-standard-costing',
                 templateUrl: 'app/modules/manual/views/standard-costing-product-creation.html',
             })
-            .state('manual-standard-costing.production-plan', {
+            .state('manual-standard-costing/production-plan', {
                 url: '/production-plan',
                 parent: 'manual-standard-costing',
                 templateUrl: 'app/modules/manual/views/standard-costing-production-plan.html',
             })
-            .state('manual-standard-costing.sales-plan', {
+            .state('manual-standard-costing/sales-plan', {
                 url: '/sales-plan',
                 parent: 'manual-standard-costing',
                 templateUrl: 'app/modules/manual/views/standard-costing-sales-plan.html',
             })
-            .state('manual-standard-costing.wip-beginning', {
+            .state('manual-standard-costing/wip-beginning', {
                 url: '/wip-beginning',
                 parent: 'manual-standard-costing',
                 templateUrl: 'app/modules/manual/views/standard-costing-wip-beginning.html',
