95ab4fb428dd34ef28717a0ebb8e1f5c74635f4a
Maxim Grishkov <grishkov.m@gmail.com>
2018-03-09 00:40:32 +0200

    added ng-bootstrap-submenu

---
+++ main/app/js/controllers/ca-drop-down-menu.component.js
@@ -0,0 +1,14 @@
+(function(){
+    'use strict';
+    angular.module('costAnswer.controllers')
+        .component('caDropDownMenu', {
+            restrict: 'E',
+            templateUrl: 'app/views/ca-drop-down-menu.html',
+            controller: caDropDownMenuController
+        });
+    function caDropDownMenuController() {
+        var vm = this;
+        
+    }
+    //caProcessProductProcessDlController.$inject = [];
+}());
